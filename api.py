10eb0b71e808391ebb37b865b36a2c71cd24ad31
Lyneca <atraguliauniliantauthr@gmail.com>
2016-09-06 21:34:23 +1000

    bugfix
    spivee doesn't know what `self` means

--- api.py
+++ api.py
@@ -28,14 +28,14 @@ class User:
 
 
 class API:
-    def refresh():
+    def refresh(self):
         self.team_name, self.team_id, self.team_domain = self._get_team_info()
         self.channels = self._get_channels()
         self.users = self._get_users()
 
     def __init__(self, token):
         self.token = token
-        refresh()
+        self.refresh()
         self.keys = {}
 
     def _get_team_info(self):
