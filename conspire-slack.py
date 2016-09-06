c18c9b2beac0eb40bb71c0f2eade26dfa8f65a5e
Lyneca <atraguliauniliantauthr@gmail.com>
2016-09-06 22:04:42 +1000

    enabled admins to signup other players for them

--- conspire-slack.py
+++ conspire-slack.py
@@ -47,7 +47,7 @@ def inform_players():
 def sign_up(message):
     text = message['text'].replace('sign up', 'signup')
     if len(text.split()) > 2 and get_user_name(message['user']) in admins:
-        user = text.split()[2]
+        user = slack.users[text.split()[2]].id
     else:
         user = message['user']
     signup.add(user)
@@ -58,7 +58,7 @@ def sign_up(message):
 def sign_down(message):
     text = message['text'].replace('sign down', 'signdown')
     if len(text.split()) > 2 and get_user_name(message['user']) in admins:
-        user = text.split()[2]
+        user = slack.users[text.split()[2]].id
     else:
         user = message['user']
     signup.remove(user)
