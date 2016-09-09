4edd97263ba6653d311ab8a62bf35a05b456ac3f
Spivee <spiveehere@gmail.com>
2016-09-09 13:44:06 +1000

    basic output
    just as a template to demonstrate how the classes work

--- classes.py
+++ classes.py
@@ -42,28 +42,32 @@ class ConspiracyData:
         self.playing.remove(player.name)
         self.eliminated.add(player.name)
         Player.eliminate(player)
-        
+    
 
 class ConspiracyGame(ConspiracyData):
-    def __init__(self, players):
+    def __init__(self, players, output):
         ConspiracyData.__init__(self, players)
         self.swapreq = set()
+        self.output = output
+        output.game_start(len(players))
+        for player in players.values():
+            output.kappa(player)
     
     def cap(self, capping, capped):
         capping = self.player(capping)
         capped = self.player(capped)
         if capping is capped.kappa():  # if the person capping X is the person who can cap X
-            self.inform_kappa_update(capped.target())
-            self.inform_capped(capping, capped)
+            self.output.capped(capped)
+            self.output.kappa_update(capped.target())
             self._eliminate(capped)
         else:
-            self.inform_kappa_update(capping.target())
-            self.inform_failed(capping, capped)
+            self.output.failed(capping, capped)
+            self.output.kappa_update(capping.target())
             self._eliminate(capping)
     
     def resign(self, player):
         player = self.player(player)
-        self.inform_resigned(player)
+        self.output.resigned(player)
         self._eliminate(player)
     
     def kswap(self, player, target, direct = False):
@@ -71,13 +75,40 @@ class ConspiracyGame(ConspiracyData):
         target = self.player(target)
         if (target.id, player.id) in self.swapreq:
             self.swapreq.remove((target.id, player.id))
-            self.inform_kswap(player, target)
-            self.inform_kswap(target, player, response=True)
+            self.output.kswap(player, target)
+            self.output.kswap(target, player, response=True)
         elif direct:
-            self.inform_kswap(player, target)
+            self.output.kswap(player, target)
         else:
             self.swapreq.add((player.id, target.id))
-            self.inform_kswap_proposal(player, target)
-        
-        
-        
+            self.output.kswap_proposal(player, target)
+
+class basic_output:  # this entire class can be passed as an output object to ConspiracyGame
+    def game_start(num):
+        print("The game started with {} players!".format(num))
+    
+    def kappa(player):
+        print("Player {p} can be capped by {k}.".format(p=player.name, k=player.kappa().name))
+    
+    def kappa_update(player):
+        print("Player {p} can now be capped by {k}.".format(p=player.name, k=player.kappa().name))
+    
+    def capped(capped):
+        print("Player {p} was capped by {k}!".format(p=capped.name, k=capped.kappa().name))
+    
+    def failed(capping, capped):
+        print("Player {k} tried to cap {p} but failed!".format(k=capping.name, p=capped.name))
+    
+    def resigned(player):
+        print("Player {p} has resigned.".format(p=player.name))
+    
+    def kswap(player, target, response=False):
+        print(("In response, player" if response else "Player") + " {p} has told player {t} that {k} can cap them.".format(p=player.name, t=target.name, k=player.kappa().name))
+    
+    def kswap_proposal(player, target):
+        print("Player {p} has proposed a kswap with {t}.".format(p=player.name, t=target.name))
+    
+    def game_end(ranks):
+        print("The game has ended! The rankings are as follows:")
+        for i, name in enumerate(ranks):
+            print("{place:>3}: {name}".format(place=i, name=name))
